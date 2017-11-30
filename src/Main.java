e57e0c2e2eced8c767f851d9e5137fdedaa6c781
Magomed Ramazanov <mramazanov@tsconsulting.ru>
2017-11-30 15:58:52 +0300

    для проверки

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,95 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Employee> employees = new ArrayList<>();
+        ArrayList<Employee> employees = null;
 //        ArrayList<Department> departments = new ArrayList<>();
 //        Set<String> departmentNames = new HashSet<>();
-        Map<String, Department> departmentsMap = new HashMap<>();
+        Map<String, Department> departmentsMap = null;
+//        BufferedReader bufferedReader = null;
+//        BufferedWriter bufferedWriter = null;
+
+        readFile(args[0],departmentsMap,employees);
+        writeFile(args[1],departmentsMap,employees);
 
 
+    }
 
-//.....чтение из файла
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(args[0]+"2"))){
-            String s;
-            while((s=bufferedReader.readLine())!=null){
-                String[] info = s.split(";");
-                employees.add(new Employee(info[0].trim(),new BigDecimal(info[2].trim())));
-                if(!departmentsMap.containsKey(info[1].trim())){
-                    departmentsMap.put(info[1].trim(),new Department(info[1].trim()));
-                    //????
-                    departmentsMap.get(info[1].trim()).addEmployee(employees.get(employees.size()-1));
+    public static void readFile(String str, Map<String,Department> departmentsMap, ArrayList<Employee> employees){
 
-                }else departmentsMap.get(info[1].trim()).addEmployee(employees.get(employees.size()-1));
+            try (BufferedReader bufferedReader =new BufferedReader(new FileReader(str))){
+
+                String s;
+                while((s=bufferedReader.readLine())!=null){
+                    String[] info = s.split(";");
+                    //employees.add(new Employee(info[0].trim(),new BigDecimal(info[2].trim())));
+                    employees = appendEmployee(employees,info[0].trim(),info[2].trim());
+                    departmentsMap = appendDepartment(departmentsMap,employees,info[1].trim());
+
+                }
+
+
+            } catch(FileNotFoundException e){
+                System.err.println("Не удаётся найти указанный файл");
+            } catch(IOException e){
+                System.err.println("Не удаётся установить соединение");
             }
 
-        } catch (FileNotFoundException e){
-            System.err.println("Не удаётся найти указанный файл");
-        } catch (IOException e) {
-            System.err.println("Не удаётся установить соединение с файлом");
-        }
+    }
 
-
-//.....запись в файл
-        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(args[1]))){
+    public static void writeFile(String str,Map<String,Department> departmentsMap, ArrayList<Employee> employees){
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(str))) {
 
             //записываем инф. о средней зарплате
             for (Department department : departmentsMap.values()) {
-                bufferedWriter.write(department.getName()+" средняя зарплата: "+department.averageSalary()+"\r\n");
+                bufferedWriter.write(department.getName() + " средняя зарплата: " + department.averageSalary() + "\r\n");
             }
 
-//.........записываем варианты распределения сотрудников по отделам
+            //.........записываем варианты распределения сотрудников по отделам
             //?????
             for (Department department : departmentsMap.values()) {
                 for (Department department1 : departmentsMap.values()) {
-                    if(!department.equals(department1)){
+                    if (!department.equals(department1)) {
                         for (Employee employee : employees) {
-                            if(department.averageSalary().compareTo(employee.getSalary() )==1&& department1.averageSalary().compareTo(employee.getSalary() )==-1)
-                                bufferedWriter.write(employee.getName()+"----> "+department1.getName()+"\r\n");
+                            if (department.averageSalary().compareTo(employee.getSalary()) == 1 && department1.averageSalary().compareTo(employee.getSalary()) == -1)
+                                bufferedWriter.write(employee.getName() + "----> " + department1.getName() + "\r\n");
 
                         }
                     }
                 }
             }
-
+        }catch (FileNotFoundException e){
+            System.err.println("Не удаётся найти указанный файл");
         }catch (IOException e){
-            e.printStackTrace();
+            System.err.println("Не удаётся установить соединение");
         }
+    }
+
+    public static ArrayList<Employee> appendEmployee(ArrayList<Employee> list, String name, String salary){
+        if(list==null)
+            list=new ArrayList<>();
+        list.add(new Employee(name, new BigDecimal(salary)));
+        return list;
 
 
+    }
+
+    public  static Map<String,Department> appendDepartment(Map<String,Department> map, ArrayList<Employee> list, String name){
+        if(map==null)
+            map = new HashMap<>();
+        if(!map.containsKey(name)){
+            map.put(name,new Department(name));
+            //System.out.println("блэт");
+            //????
+            map.get(name).addEmployee(list.get(list.size()-1));
+
+        }else map.get(name).addEmployee(list.get(list.size()-1));
+
+
+        return map;
 
     }
 
