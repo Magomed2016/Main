c15bba4e7fe87e4dbdf8cb591f6ae1517019119b
Magomed Ramazanov <mramazanov@tsconsulting.ru>
2017-12-01 19:26:41 +0300

    01.12.2017

--- src/Main.java
+++ src/Main.java
@@ -5,114 +5,83 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Employee> employees = null;
-//        ArrayList<Department> departments = new ArrayList<>();
-//        Set<String> departmentNames = new HashSet<>();
-        Map<String, Department> departmentsMap = null;
-        ArrayList<Object> objectArrayList = null;
-//        BufferedReader bufferedReader = null;
-//        BufferedWriter bufferedWriter = null;
-
-        objectArrayList = readFile(args[0],departmentsMap,employees);
-        employees = (ArrayList<Employee>) objectArrayList.get(0);
-        departmentsMap = (Map<String, Department>) objectArrayList.get(1);
-        writeFile(args[1],departmentsMap,employees);
-
+        writeFile(readFile(args[0]),args[1]);
 
     }
 
-    public static ArrayList<Object> readFile(String str, Map<String,Department> departmentsMap, ArrayList<Employee> employees){
-            ArrayList<Object> listObject = new ArrayList<>();
-            try (BufferedReader bufferedReader =new BufferedReader(new FileReader(str))){
+    public static Set<Department> readFile(String url){
 
-                String s;
-                while((s=bufferedReader.readLine())!=null){
-                    String[] info = s.split(";");
-                    //employees.add(new Employee(info[0].trim(),new BigDecimal(info[2].trim())));
-                    employees = appendEmployee(employees,info[0].trim(),info[2].trim());
-                    departmentsMap = appendDepartment(departmentsMap,employees,info[1].trim());
+            Set<Department> departmentSet = new HashSet<>();
 
-                }
+            try(BufferedReader bufferedReader = new BufferedReader(new FileReader(url))){
 
-
-            } catch(FileNotFoundException e){
-                System.err.println("Не удаётся найти указанный файл");
-            } catch(IOException e){
-                System.err.println("Не удаётся установить соединение");
-            }
-
-        listObject.add(employees);
-        listObject.add(departmentsMap);
-
-        return listObject;
-
-
-
-    }
-
-    public static void writeFile(String str,Map<String,Department> departmentsMap, ArrayList<Employee> employees){
-
-        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(str))) {
-
-            //записываем инф. о средней зарплате
-            for (Department department : departmentsMap.values()) {
-                bufferedWriter.write(department.getName() + " средняя зарплата: " + department.averageSalary() + "\r\n");
-            }
-
-            //.........записываем варианты распределения сотрудников по отделам
-            //?????
-            for (Department department : departmentsMap.values()) {
-                for (Department department1 : departmentsMap.values()) {
-                    if (!department.equals(department1)) {
-                        for (Employee employee : employees) {
-                            if (department.averageSalary().compareTo(employee.getSalary()) == 1 && department1.averageSalary().compareTo(employee.getSalary()) == -1)
-                                bufferedWriter.write(employee.getName() + "----> " + department1.getName() + "\r\n");
-
-                        }
+                String string;
+                while((string=bufferedReader.readLine())!=null) {
+                    String[] info = string.split(";");
+                    if(string.isEmpty()) continue;
+                    if(info[0].trim().isEmpty() || info[1].trim().isEmpty() || info[2].trim().isEmpty() || info[2].trim().startsWith("-")){
+                        throw new IllegalArgumentException();
                     }
+                    Employee employee = new Employee(info[0].trim(),new BigDecimal(info[2].trim()));
+                    departmentSet.add(new Department(info[1].trim()));
+                    for (Department department : departmentSet) {
+                        if(department.getName().equals(info[1].trim()))
+                            department.addEmployee(employee);
+                    }
+
                 }
+
+            }catch (FileNotFoundException e ){
+                System.err.println("Неверный путь к файлу");
+
+            }catch (IOException e){
+                System.err.println("Не удаётся установить соединение");
+
+            }catch (NumberFormatException e){
+                System.err.println("Неверный формат данных");
+            }catch (IllegalArgumentException e){
+                System.err.println("Считанные данные не допустимы");
             }
-        }catch (FileNotFoundException e){
-            System.err.println("Не удаётся найти указанный файл");
-        }catch (IOException e){
-            System.err.println("Не удаётся установить соединение");
-        }catch (NullPointerException e){
-            System.err.println("Передано пустое множество");
-        }
-
-
-
+            return departmentSet;
     }
 
-    public static ArrayList<Employee> appendEmployee(ArrayList<Employee> list, String name, String salary){
-        if(list==null)
-            list=new ArrayList<>();
-        try {
-            list.add(new Employee(name, new BigDecimal(salary)));
-        }catch (NumberFormatException e){
-            System.err.println("Указан не норректный формат данных");
-        }
-        return list;
+    public static void writeFile(Set<Department> departmentSet, String url){
 
+            try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(url))){
 
-    }
+                for (Department department : departmentSet) {
+                    bufferedWriter.write(department.getName()+" средняя зарплата: "+department.averageSalary()+"\r\n");
+                }
+                bufferedWriter.write(regroupingEmployees(departmentSet));
 
-    public  static Map<String,Department> appendDepartment(Map<String,Department> map, ArrayList<Employee> list, String name){
-        if(map==null)
-            map = new HashMap<>();
-        if(!map.containsKey(name)){
-            map.put(name,new Department(name));
-            //????
-            try {
-                map.get(name).addEmployee(list.get(list.size() - 1));
+            }catch (FileNotFoundException e ){
+                System.err.println("Некорректный путь к файлу");
+
+            }catch (IOException e){
+                System.err.println("Не удаётся установить соединение");
+
             }catch (NullPointerException e){
-                System.err.println("Передан пустой список");
+                System.err.println("Передано пустое множество");
             }
-        }else map.get(name).addEmployee(list.get(list.size()-1));
 
 
-        return map;
+    }
 
+    public static String regroupingEmployees(Set<Department> departmentSet){
+        StringBuilder stringBuilder = new StringBuilder("");
+        for (Department department : departmentSet) {
+            for (Department department1 : departmentSet) {
+                if(!department.equals(department1))
+                    for (Employee employee : department.getEmployeeList()) {
+                        if (department.averageSalary().compareTo(employee.getSalary()) == 1 && department1.averageSalary().compareTo(employee.getSalary()) == -1)
+                            stringBuilder.append(employee.getName()+"----> "+ department1.getName()+"\r\n");
+
+                    }
+
+            }
+
+        }
+        return String.valueOf(stringBuilder);
     }
 
 }
