2bcef115be11a5e81f59cdc46e039125e41ad3c8
Magomed Ramazanov <mramazanov@tsconsulting.ru>
2017-12-01 19:29:18 +0300

    01.12.2017

--- src/Department.java
+++ src/Department.java
@@ -1,12 +1,15 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 
 public class Department {
     private String name;
-    private ArrayList<Employee> list;
+    private ArrayList<Employee> employeeList;
 
     public Department(String name) {
+
         this.name = name;
+
     }
 
     public String getName() {
@@ -17,35 +20,49 @@ public class Department {
         this.name = name;
     }
 
-    public ArrayList<Employee> getList() {
-        if(list==null)
-            list = new ArrayList<>();
-        return list;
+    public ArrayList<Employee> getEmployeeList() {
+        if(employeeList ==null)
+            employeeList = new ArrayList<>();
+        return employeeList;
     }
 
-    public void setList(ArrayList<Employee> list) {
-        this.list = list;
+    public void setEmployeeList(ArrayList<Employee> employeeList) {
+        this.employeeList = employeeList;
     }
 
     public BigDecimal averageSalary(){
 
         BigDecimal averageSalary = new BigDecimal("0.00");
-        for (Employee employee : list) {
+        for (Employee employee : employeeList) {
             averageSalary = averageSalary.add(employee.getSalary());
         }
-        return averageSalary.divide(new BigDecimal(list.size()),2,BigDecimal.ROUND_UP);
+        return averageSalary.divide(new BigDecimal(getEmployeeList().size()),2, RoundingMode.HALF_UP);
 
     }
 
     public void addEmployee(Employee employee){
-        getList().add(employee);
+        getEmployeeList().add(employee);
     }
 
     public void printInfo(){
         System.out.println(name);
-        for (Employee employee : list) {
+        for (Employee employee : employeeList) {
             System.out.println("----------"+employee.getName());
 
         }
+    }
+
+    @Override
+    public int hashCode() {
+        return getName().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Department department = (Department)obj;
+        if(getName().equals(department.getName()))
+            return true;
+
+        else return super.equals(obj);
     }
 }
