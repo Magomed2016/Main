40c41a23360592df63d1e85d9d5d686d7a8a5e12
Magomed Ramazanov <mramazanov@tsconsulting.ru>
2017-12-05 19:01:09 +0300

    привязка алгоритма

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,57 @@ public class Main {
     public static void main(String[] args) {
 
         Set<Department> departments = null;
+        ArrayList<Employee> employees = new ArrayList<>();
 
-        writeFile(readFile(departments, args[0]),args[1]);
+        departments = readFile(departments, args[0]);
+
+        writeFile(departments,args[1]);
+
+        for (Department department : departments) {
+            for (Department department1 : departments) {
+                if(!department.equals(department1)) {
+                    for (int i = 1; i <= department.getEmployeeList().size(); i++) {
+                        algoritm(department, department1, department.getEmployeeList(),employees,i,0);
+
+                    }
+                    System.out.println("");
+                }
+            }
+
+        }
 
     }
+
+    public static void algoritm(Department department, Department department1, ArrayList<Employee> employeesOfDepartment, ArrayList<Employee> employeeArrayList, int level, int nextPosition){
+        if(employeeArrayList.size()==level){
+            BigDecimal salary = new BigDecimal("0.00");
+            StringBuilder stringBuilder = new StringBuilder("");
+            for (Employee employee : employeeArrayList) {
+
+                salary = salary.add(employee.getSalary());
+                stringBuilder.append(employee.getName()+" ");
+            }
+
+            if(department.averageSalary().compareTo(salary )==1 && department1.averageSalary().compareTo(salary)==-1){
+                System.out.println(String.valueOf(stringBuilder)+"----> "+department1.getName());
+            }
+            return;
+        }
+
+
+        for(int j= employeesOfDepartment.indexOf(employeesOfDepartment.get(nextPosition)); j<employeesOfDepartment.size(); j++){
+            if(!employeeArrayList.contains(employeesOfDepartment.get(j))){
+                employeeArrayList.add(employeesOfDepartment.get(j));
+                algoritm(department, department1,employeesOfDepartment,employeeArrayList,level,nextPosition);
+                employeeArrayList.remove(employeesOfDepartment.get(j));
+            }
+            nextPosition++;
+        }
+
+
+    }
+
+    public static void chek(){}
 
     public static Set<Department> readFile(Set<Department> departmentSet, String url){
 
@@ -79,17 +126,20 @@ public class Main {
         StringBuilder stringBuilder = new StringBuilder("");
         for (Department department : departmentSet) {
             for (Department department1 : departmentSet) {
-                if(!department.equals(department1))
+                if(!department.equals(department1)) {
                     for (Employee employee : department.getEmployeeList()) {
                         if (department.averageSalary().compareTo(employee.getSalary()) == 1 && department1.averageSalary().compareTo(employee.getSalary()) == -1)
-                            stringBuilder.append(employee.getName()+"----> "+ department1.getName()+"\r\n");
+                            stringBuilder.append(employee.getName() + "----> " + department1.getName() + "\r\n");
 
                     }
-
+                }
             }
 
         }
         return String.valueOf(stringBuilder);
     }
+
+
+
 
 }
